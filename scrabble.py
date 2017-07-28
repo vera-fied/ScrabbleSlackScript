de2f2400b36a6617ea058b4098041aa73a1dd71b
Ben Brubaker <ben.brubaker@nextgearcapital.com>
2017-07-28 13:42:01 -0400

    Fixed colons

--- scrabble.py
+++ scrabble.py
@@ -6,7 +6,7 @@ def convert(char, flag):
 	if (char == ' ' and flag):
 		return ":space:"
 	elif char == ' ':
-		return "scrabble-blank"
+		return ":scrabble-blank:"
 	elif char.isalpha():
 		if (flag and (char != 'a' and char != 'b' and char != 'o' and char != 'x' and char != 's' and char != 'm' and char != 'v')):
 			return ":" + char + ":"
