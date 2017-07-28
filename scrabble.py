ca7043a80073652adacacbee95dd00c03c08c824
Ben Brubaker <ben.brubaker@nextgearcapital.com>
2017-07-28 13:12:29 -0400

    Added

---
+++ scrabble.py
@@ -0,0 +1,23 @@
+import sys
+
+def convert(char, flag):
+	if (char == ' ' and flag):
+		return ":space:"
+	elif char == ' ':
+		return "scrabble-blank"
+	elif char.isalpha():
+		if (flag and (char != 'a' and char != 'b' and char != 'o' and char != 'x')):
+			return ":" + char + ":"
+		else:
+			return ":scrabble-" + char + ":"
+	else:
+		return char
+
+output = ""
+input = sys.argv[1].lower()
+aliasFlag = (sys.argv[2].lower() != "false")
+
+for ch in input:
+	output += convert(ch, aliasFlag)
+
+print(output)
